a0a0a4f14036a3026b2ed9ea8b73bb4866be3e08
Zhebel Alika <rezedawasser@gmail.com>
2022-05-27 23:57:15 +0300

    Created

---
+++ Python.py
@@ -0,0 +1,11 @@
+stats = {'facebook': 55, 'yandex': 120, 'vk': 115, 'google': 99, 'email': 42, 'ok': 98}
+
+max = 0
+
+for key, value in stats.items():
+  if value > max:
+    max = value
+    name = key
+print(f'Канал с максимальным объемом продаж: {name}')
+
+print(max(stats, key=stats.get)) # версия преподавателя Александра Бардина
